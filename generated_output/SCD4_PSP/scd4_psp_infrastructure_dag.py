e9dbb34bb43f1e75297714612348f88d5ef3b113
Billy Newport <billy@billynewport.com>
2026-06-08 08:41:05 -0400

    Pin DataSurface image to v1.4.40

--- generated_output/SCD4_PSP/scd4_psp_infrastructure_dag.py
+++ generated_output/SCD4_PSP/scd4_psp_infrastructure_dag.py
@@ -602,7 +602,7 @@ merge_task = KubernetesPodOperator(
     task_id='infrastructure_merge_task',
     name='scd4-psp-infra-merge',
     namespace='ds-scale',
-    image='registry.gitlab.com/datasurface-inc/datasurface/datasurface:v1.4.39',
+    image='registry.gitlab.com/datasurface-inc/datasurface/datasurface:v1.4.40',
     cmds=['/bin/bash'],
     arguments=[
         '-c',
